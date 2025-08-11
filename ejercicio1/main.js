34d90e280b9442a175f9c49d92ed501c958421c3
Jonathan Xavier Perez Camacho <89760826+joprzc@users.noreply.github.com>
2025-08-11 02:55:05 +0000

    ejercicios logica de programacion

---
+++ ejercicio1/main.js
@@ -0,0 +1,28 @@
+txt.addEventListener('input', (event) => {
+    let text = event.target.value;
+
+    // omitir signos
+    text = text.replace(/[$\.¿\?,;]/g, " ");
+
+    // omitir espacios
+    text = text.replace(/\s+/g, " ").trim();
+
+    // convertir a mayusculas
+    text = text.toUpperCase();
+
+    // convertir un String a un Array a partir de un caracter(en este caso " ")
+    const words = text.split(" ");
+
+    // buscar palabras repetidas
+    const resultWords = words.reduce((ac, word) => {
+        ac.filter(w=>w.word === word).length === 0 ?
+            ac.push({
+                word: word, total: 1
+            }) :
+            ac.find(w=>w.word === word).total++
+
+            return ac;
+    }, []);
+
+    console.log(resultWords);
+});
